57e297656b29b9d8126c45047ea4cea1187c6dc6
WG-19 <wadzagute@gmail.com>
2025-08-15 13:19:14 +0200

    feat: add Google Analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useLocation } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 
+// Track page views when the route changes
+function PageViewTracker() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    // Send pageview to Google Analytics
+    window.gtag('config', 'G-0GX0455CN6', {
+      page_path: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartProvider>
-      <App />
+      <App>
+        <PageViewTracker />
+      </App>
     </CartProvider>
   </StrictMode>,
 )
